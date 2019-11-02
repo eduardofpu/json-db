c543a44a58fbd30ef5a7587932ecad493da6ffdb
eduardo <eduardo27_minotauro>
2019-11-02 16:25:52 -0300

    index

---
+++ index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const express = require("express");
+const morgan = require("db.json");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+
+/**
+ * Database setup
+ */
+mongoose.connect(
+  process.env.REACT_APP_API_DB_JSON_URL,
+  {
+    useNewUrlParser: true
+  }
+);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(
+  "/users",
+  express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
+);
+
+app.listen(process.env.PORT || 3001);
